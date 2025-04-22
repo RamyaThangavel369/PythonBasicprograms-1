1475e792f48b5674ab8fc3185830d8fc8c3dd962
ramya <@RamyaThangavel369>
2025-04-22 15:35:25 -0700

    concatenation

---
+++ concatenate.py
@@ -0,0 +1,21 @@
+#using +operator
+"""
+string1="Hello"
+string2="World"
+result=string1+" "+string2
+print(result)"""
+
+#using join()
+"""
+string=["Hello" ,"Python" ,"World"]
+result=" ".join(string)
+print(result)"""
+
+#f-string
+"""
+string1="Hello"
+string2="World"
+result=f"{string1} {string2}"
+print(result)"""
+
+
