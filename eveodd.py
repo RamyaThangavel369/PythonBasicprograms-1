085b8e6baff7fae302c0ed37f46daf2ea54cdaf2
ramya <@RamyaThangavel369>
2025-04-22 15:32:33 -0700

    Even or odd

---
+++ eveodd.py
@@ -0,0 +1,85 @@
+
+
+
+def checkoddoreven(a):
+    if a%2==0:
+      print("a is even number")
+    elif a%2!=0:
+      print("a is odd number")
+
+checkoddoreven(2)
+checkoddoreven(5)
+"""
+x,y,z=input("Enter three values:").split()
+print("Number of students:", x)
+print("Number of boys:", y)
+print("Number of girls:", z)
+
+
+age_input=input("Enter your age:")
+age=int(age_input)
+if age<0:
+    print("Please enter a valid age:")
+elif age<18:
+    print("You are a minor")
+elif age>=18 and age<65:
+     print("You are an adult")
+else:
+      print("You are a senior citizen")
+
+color=input("What is the color of rose?:")
+print(color)
+Print Numbers in Python
+color=int(input("How many roses are there?:"))
+print(color)
+price=float(input("what  is the prices of rose?:"))
+print(price)
+a = "Hello World"
+b = 10
+c = 11.22
+d = ("Geeks", "for", "Geeks")
+e = ["Geeks", "for", "Geeks"]
+f = {"Geeks": 1, "for":2, "Geeks":3}
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+
+amount=50.00
+print("amount:${:.2f}".format(amount))
+
+Name='Ramya'
+Age=34
+print(f"Hello,Myself {Name},I'm {Age}years old.")
+
+print("Python",end='@')
+print("Developer")
+print('k','R','A','A',sep='')
+print('04','02','2025',sep='-')
+print("Ramya",'gmail.com',sep='@')
+
+add=int(input("Enter the value"))
+num = add + 5
+print("The sum of value %d"%num)"""
+from email.header import BSPACE
+
+
+a=("hello")
+b=("world")
+print(a+b)
+"""without space"""
+"""
+a=("hello")
+b=("world")
+print(a+"  "+b)
+with space
+a=("hello")
+b=("world")
+print( a+ " " + b ,sep="@")
+a=("hello")
+b=("world")
+print("hello","world",sep='@')
+"""
+
