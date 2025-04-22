ad3fea0adf7dc3f19d31e816b5d9955a5b0a0855
ramya <@RamyaThangavel369>
2025-04-22 15:37:56 -0700

    list

---
+++ List.py
@@ -0,0 +1,16 @@
+#List-used to store multiple items in a single
+
+food=["Burger","Hot dogs","Pizza","Spaghetti","Pasta","Noodles"]
+#print(food[4])
+food[0]="Burger"
+#food.append("icecream")
+#food.remove("Hot dogs")
+#food.pop()
+#food.insert(5,"cake")
+#food.reverse()
+#food.sort()
+#food.clear()
+
+
+for k in food:
+    print(k)
