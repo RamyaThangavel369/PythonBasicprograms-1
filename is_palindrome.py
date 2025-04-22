ba980ca82fa5944a652487fbd5bc0471ba97f741
RamyaThangavel369 <ramyakrishna.usa@gmail.com>
2025-04-22 15:40:45 -0700

    Delete is_palindrome.py

--- is_palindrome.py
+++
@@ -1,8 +0,0 @@
-def is_palindrome(s):
-    if len(s)<2:
-     return True
-
-    if s[0]!=s[-1]:
-        return False
-    return is_palindrome(s[1:-1])   #if the first and last characters match,it's palindrome.
-print(is_palindrome("hello"))       #if the first and last characters don't match,it's not palindrome.
