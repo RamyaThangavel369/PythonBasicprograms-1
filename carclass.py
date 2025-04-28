bc23bb52d269c9d2d0ecae7116ff298b8bdaf9f8
ramya <@RamyaThangavel369>
2025-04-28 12:22:35 -0700

    Class car program

---
+++ carclass.py
@@ -0,0 +1,24 @@
+from classobj import car
+vehicle1=car("Tesla","Red","2023","Hybrid")
+vehicle2=car("Audi","Black","2021","Suv")
+print(vehicle1.make)
+print(vehicle1.color)
+print(vehicle1.year)
+print(vehicle1.model)
+print(vehicle2.make)
+print(vehicle2.color)
+print(vehicle2.year)
+print(vehicle2.model)
+
+vehicle1.drive1()
+vehicle1.stop1()
+
+vehicle2.drive2()
+vehicle2.stop2()
+
+
+
+
+
+
+
