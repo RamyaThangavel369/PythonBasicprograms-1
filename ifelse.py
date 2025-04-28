ed5213978732488128daaa68df069518b3f7d737
ramya <@RamyaThangavel369>
2025-04-28 12:16:28 -0700

    If else Statement

---
+++ ifelse.py
@@ -0,0 +1,14 @@
+age=int(input("Enter your age:"))
+if age >=18 and age<=64 : #above 18years
+    print("You are signed up now!")
+elif age<=18 and age>=3: #below 18years
+    print("You are not eligible!")
+elif age>=65:
+    print("You are senior citizen")
+else:
+    print("You are child")
+
+
+
+
+
