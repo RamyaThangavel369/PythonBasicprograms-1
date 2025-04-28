29e0de044ce638fe538a8c197b7f38cbb5e71ede
ramya <@RamyaThangavel369>
2025-04-28 11:40:09 -0700

    compare the two values

---
+++ compare_values.py
@@ -0,0 +1,44 @@
+#Compare the two values
+def compare_values(a,b):
+    print(a==b)
+    print(a!=b)
+    print(a<b)
+    print(a>b)
+compare_values(6,9)
+
+#compare(!=)
+string1="Hello"
+string2="World"
+if string1!=string2:
+    print("The strings are not equal")
+else:
+    print("The strings are  equal")
+
+#compare(==)
+
+string1 = "Hello"
+string2 = "World"
+if string1 == string2:
+        print("The strings are  equal")
+else:
+        print("The strings are not equal")
+
+#compare(>)greater than
+
+string1 = "Hello"
+string2 = "World"
+if string1>string2:
+    print("The strings are equal")
+else:
+    print("The strings are not equal")  #The strings are not equal
+
+
+#compare(<)less than
+
+string1 = "Hello"
+string2 = "World"
+if string1<string2:
+    print("The strings are equal")
+else:
+    print("The strings are not equal")  #The strings are equal
+
