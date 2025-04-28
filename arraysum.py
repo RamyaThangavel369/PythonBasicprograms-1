a3489c3fbb444d84bad27fbc41ad0c0110c0c147
ramya <@RamyaThangavel369>
2025-04-28 13:11:24 -0700

    sum of elements using Array

---
+++ arraysum.py
@@ -0,0 +1,9 @@
+#write the program to find the sum of elements in a list?
+
+def Arrays_list():
+    list=[23,56,74,43,21]
+    sum=0
+    for i in list:
+          sum=sum+i
+    print("Sum of list is",sum)
+Arrays_list()
