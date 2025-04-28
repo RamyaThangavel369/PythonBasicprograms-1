dba25b04dd6e1e7d6875892f5727cb8635e9d55d
ramya <@RamyaThangavel369>
2025-04-28 12:38:12 -0700

    Comment function program

---
+++ comment.py
@@ -0,0 +1,14 @@
+def comment_function():
+    #print("Welcome to the world")
+    print("The beautiful flower")
+
+    print("Python programming")
+    print("The days are good")
+
+comment_function()
+
+
+
+
+
+
