7d0453a71d1ee72f94e884e1efa687454a0f9cc1
ramya <@RamyaThangavel369>
2025-04-28 11:58:30 -0700

    checkvalues using python function

---
+++ checkvalue.py
@@ -0,0 +1,19 @@
+
+def checkvalue():
+  flag = False
+  a=1
+  b=1
+
+  if a == 1 or b == -1:
+      print("The flag is true")
+  elif a==-1 or b==-1:
+       print("The flag is true")
+  elif a==1 and b==2:
+       print("The flag is true")
+  else:
+        print("The flag is false")
+checkvalue()
+
+
+
+
